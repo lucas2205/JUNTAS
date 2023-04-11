004780f889e52de94ceb5eaa1e8b96ba46df9030
lucas2205 <76697605+lucas2205@users.noreply.github.com>
2023-04-11 09:37:43 -0300

    Update data.sql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -2,5 +2,5 @@
 
 --SET SESSION sql_mode='NO_AUTO_VALUE_ON_ZERO';
 
-INSERT INTO `roles` (`name`) VALUES ('USER');
-INSERT INTO `roles` (`name`) VALUES ('ADMIN');
+--INSERT INTO `roles` (`name`) VALUES ('USER');
+--INSERT INTO `roles` (`name`) VALUES ('ADMIN');
